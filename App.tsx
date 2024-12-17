be68b7137b09d86825e765879705ba088fda8dfd
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-12-17 23:17:55 +0530

    feat: project-complete

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { AuthProvider } from "./contexts/AuthContext";
+import AuthScreen from "./screens/AuthScreen";
+import FeedScreen from "./screens/FeedScreen";
+import OTPScreen from "./screens/OTPScreen";
+import ReviewFormScreen from "./screens/ReviewFormScreen";
+import { RootStackParamList } from "./types";
+// import CameraScreen from "./screens/CameraScreen";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<AuthProvider>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Auth">
+					<Stack.Screen
+						name="Auth"
+						component={AuthScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="OTP"
+						component={OTPScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Feed"
+						component={FeedScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="ReviewForm"
+						component={ReviewFormScreen}
+						options={{ title: "Write a Review" }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</AuthProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
